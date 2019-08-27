4aaee427237395016521357fa3ea7b2c6b40cf6b
Lucas Sandeville <lucas.sandeville@gmail.com>
2019-08-27 18:53:09 -0300

    overpopulate the database and fix minor validations problems

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-# specialties = ["Abdominal Radiology", "Addiction Psychiatry", "Adolescent Medicine", "Adult Cardiothoracic Anesthesiology", "Adult Reconstructive Orthopaedics", "Advanced Heart Failure & Transplant Cardiology", "Allergy & Immunology", "Anesthesiology", "Biochemical Genetics", "Blood Banking - Transfusion Medicine", "Cardiothoracic Radiology", "Cardiovascular Disease", "Chemical Pathology", "Child & Adolescent Psychiatry", "Child Abuse Pediatrics", "Child Neurology", "Clinical & Laboratory Immunology", "Clinical Cardiac Electrophysiology", "Clinical Neurophysiology", "Colon & Rectal Surgery", "Congenital Cardiac Surgery", "Craniofacial Surgery", "Critical Care Medicine", "Critical Care Medicine", "Cytopathology", "Dermatology", "Dermatopathology", "Developmental-Behavioral Pediatrics", "Emergency Medicine", "Endocrinology, Diabetes & Metabolism", "Endovascular Surgical Neuroradiology", "Endovascular Surgical Neuroradiology", "Endovascular Surgical Neuroradiology", "Family Medicine", "Female Pelvic Medicine & Reconstructive Surgery", "Foot & Ankle Orthopaedics", "Forensic Pathology", "Forensic Psychiatry", "Gastroenterology", "Geriatric Medicine", "Geriatric Medicine", "Geriatric Medicine", "Geriatric Psychiatry", "Hand Surgery", "Hand Surgery", "Hand Surgery", "Hematology", "Hematology", "Hematology & Oncology", "Infectious Disease", "Internal Medicine", "Internal Medicine-Pediatrics", "Interventional Cardiology", "Medical Genetics", "Medical Microbiology", "Medical Toxicology", "Medical Toxicology", "Molecular Genetic Pathology", "Muscoskeletal Radiology", "Musculoskeletal Oncology", "Neonatal-Perinatal Medicine", "Nephrology", "Neurological Surgery", "Neurology", "Neuromuscular Medicine", "Neuromuscular Medicine", "Neuropathology", "Neuroradiology", "Nuclear Medicine", "Nuclear Radiology", "Obstetric Anesthesiology", "Obstetrics & Gynecology", "Oncology", "Ophthalmic Plastic & Reconstructive Surgery", "Ophthalmology", "Orthopaedic Sports Medicine", "Orthopaedic Surgery", "Orthopaedic Surgery of the Spine", "Orthopaedic Trauma", "Otolaryngology", "Otology - Neurotology", "Pain Medicine", "Pain Medicine", "Pain Medicine", "Pathology-Anatomic & Clinical", "Pediatric Anesthesiology", "Pediatric Cardiology", "Pediatric Critical Care Medicine", "Pediatric Emergency Medicine", "Pediatric Emergency Medicine", "Pediatric Endocrinology", "Pediatric Gastroenterology", "Pediatric Hematology-Oncology", "Pediatric Infectious Diseases", "Pediatric Nephrology", "Pediatric Orthopaedics", "Pediatric Otolaryngology", "Pediatric Pathology", "Pediatric Pulmonology", "Pediatric Radiology", "Pediatric Rheumatology", "Pediatric Sports Medicine", "Pediatric Surgery", "Pediatric Transplant Hepatology", "Pediatric Urology", "Pediatrics", "Physical Medicine & Rehabilitation", "Plastic Surgery", "Preventive Medicine", "Procedural Dermatology", "Psychiatry", "Pulmonary Disease", "Pulmonary Disease & Critical Care Medicine", "Radiation Oncology", "Radiology-Diagnostic", "Rheumatology", "Sleep Medicine", "Spinal Cord Injury Medicine", "Sports Medicine", "Sports Medicine", "Sports Medicine", "Sports Medicine", "Surgery-General", "Surgical Critical Care", "Thoracic Surgery", "Thoracic Surgery-Integrated", "Transplant Hepatology", "Urology", "Vascular & Interventional Radiology", "Vascular Surgery", "Allergy & Immunology", "Clinical & Laboratory Immunology", "Anesthesiology", "Adult Cardiothoracic Anesthesiology", "Critical Care Medicine", "Obstetric Anesthesiology", "Pain Medicine", "Pediatric Anesthesiology", "Colon & Rectal Surgery", "Dermatology", "Dermatopathology", "Procedural Dermatology", "Emergency Medicine", "Medical Toxicology", "Pediatric Emergency Medicine", "Sports Medicine", "Family Medicine", "Geriatric Medicine", "Sports Medicine", "Geriatric Medicine", "Internal Medicine", "Advanced Heart Failure & Transplant Cardiology", "Cardiovascular Disease", "Clinical Cardiac Electrophysiology", "Critical Care Medicine", "Endocrinology, Diabetes & Metabolism", "Gastroenterology", "Geriatric Medicine", "Hematology", "Hematology & Oncology", "Infectious Disease", "Interventional Cardiology", "Nephrology", "Oncology", "Pulmonary Disease", "Pulmonary Disease & Critical Care Medicine", "Rheumatology", "Sports Medicine", "Transplant Hepatology", "Internal Medicine-Pediatrics", "Medical Genetics", "Biochemical Genetics", "Molecular Genetic Pathology", "Neurological Surgery", "Endovascular Surgical Neuroradiology", "Neurology", "Child Neurology", "Clinical Neurophysiology", "Endovascular Surgical Neuroradiology", "Neuromuscular Medicine", "Pain Medicine", "Nuclear Medicine", "Obstetrics & Gynecology", "Female Pelvic Medicine & Reconstructive Surgery", "Ophthalmology", "Ophthalmic Plastic & Reconstructive Surgery", "Orthopaedic Surgery", "Adult Reconstructive Orthopaedics", "Foot & Ankle Orthopaedics", "Hand Surgery", "Musculoskeletal Oncology", "Orthopaedic Sports Medicine", "Orthopaedic Surgery of the Spine", "Orthopaedic Trauma", "Pediatric Orthopaedics", "Otolaryngology", "Otology - Neurotology", "Pediatric Otolaryngology", "Pathology-Anatomic & Clinical", "Blood Banking - Transfusion Medicine", "Chemical Pathology", "Cytopathology", "Forensic Pathology", "Hematology", "Medical Microbiology", "Neuropathology", "Pediatric Pathology", "Pediatrics", "Adolescent Medicine", "Child Abuse Pediatrics", "Developmental-Behavioral Pediatrics", "Neonatal-Perinatal Medicine", "Pediatric Cardiology", "Pediatric Critical Care Medicine", "Pediatric Emergency Medicine", "Pediatric Endocrinology", "Pediatric Gastroenterology", "Pediatric Hematology-Oncology", "Pediatric Infectious Diseases", "Pediatric Nephrology", "Pediatric Pulmonology", "Pediatric Rheumatology", "Pediatric Sports Medicine", "Pediatric Transplant Hepatology", "Physical Medicine & Rehabilitation", "Neuromuscular Medicine", "Pain Medicine", "Spinal Cord Injury Medicine", "Sports Medicine", "Plastic Surgery", "Craniofacial Surgery", "Hand Surgery", "Preventive Medicine", "Medical Toxicology", "Psychiatry", "Addiction Psychiatry", "Child & Adolescent Psychiatry", "Forensic Psychiatry", "Geriatric Psychiatry", "Radiation Oncology", "Radiology-Diagnostic", "Abdominal Radiology", "Cardiothoracic Radiology", "Endovascular Surgical Neuroradiology", "Muscoskeletal Radiology", "Neuroradiology", "Nuclear Radiology", "Pediatric Radiology", "Vascular & Interventional Radiology", "Sleep Medicine", "Surgery-General", "Hand Surgery", "Pediatric Surgery", "Surgical Critical Care", "Vascular Surgery", "Thoracic Surgery", "Congenital Cardiac Surgery", "Thoracic Surgery-Integrated", "Urology", "Pediatric Urology"]
+require 'date'
+require 'faker'
 
 puts 'Deleting data from database'
 
@@ -18,9 +18,13 @@ Specialty.destroy_all
 # Doctor.destroy_all
 User.destroy_all
 
-puts 'Creating new data'
 
-cardiology = Specialty.create!(name: 'Cardiology')
+puts 'Creating fulano, ciclana, beltrana and futrano'
+puts '----------------------------------------------------------'
+
+all_specialties = ["Abdominal Radiology", "Addiction Psychiatry", "Adolescent Medicine", "Adult Cardiothoracic Anesthesiology", "Adult Reconstructive Orthopaedics", "Advanced Heart Failure & Transplant Cardiology", "Allergy & Immunology", "Anesthesiology", "Biochemical Genetics", "Blood Banking - Transfusion Medicine", "Cardiothoracic Radiology", "Cardiology", "Cardiovascular Disease", "Chemical Pathology", "Child & Adolescent Psychiatry", "Child Abuse Pediatrics", "Child Neurology", "Clinical & Laboratory Immunology", "Clinical Cardiac Electrophysiology", "Clinical Neurophysiology", "Colon & Rectal Surgery", "Congenital Cardiac Surgery", "Craniofacial Surgery", "Critical Care Medicine", "Critical Care Medicine", "Cytopathology", "Dermatology", "Dermatopathology", "Developmental-Behavioral Pediatrics", "Emergency Medicine", "Endocrinology, Diabetes & Metabolism", "Endovascular Surgical Neuroradiology", "Endovascular Surgical Neuroradiology", "Endovascular Surgical Neuroradiology", "Family Medicine", "Female Pelvic Medicine & Reconstructive Surgery", "Foot & Ankle Orthopaedics", "Forensic Pathology", "Forensic Psychiatry", "Gastroenterology", "Geriatric Medicine", "Geriatric Medicine", "Geriatric Medicine", "Geriatric Psychiatry", "Hand Surgery", "Hand Surgery", "Hand Surgery", "Hematology", "Hematology", "Hematology & Oncology", "Infectious Disease", "Internal Medicine", "Internal Medicine-Pediatrics", "Interventional Cardiology", "Medical Genetics", "Medical Microbiology", "Medical Toxicology", "Medical Toxicology", "Molecular Genetic Pathology", "Muscoskeletal Radiology", "Musculoskeletal Oncology", "Neonatal-Perinatal Medicine", "Nephrology", "Neurological Surgery", "Neurology", "Neuromuscular Medicine", "Neuromuscular Medicine", "Neuropathology", "Neuroradiology", "Nuclear Medicine", "Nuclear Radiology", "Obstetric Anesthesiology", "Obstetrics & Gynecology", "Oncology", "Ophthalmic Plastic & Reconstructive Surgery", "Ophthalmology", "Orthopaedic Sports Medicine", "Orthopaedic Surgery", "Orthopaedic Surgery of the Spine", "Orthopaedic Trauma", "Otolaryngology", "Otology - Neurotology", "Pain Medicine", "Pain Medicine", "Pain Medicine", "Pathology-Anatomic & Clinical", "Pediatric Anesthesiology", "Pediatric Cardiology", "Pediatric Critical Care Medicine", "Pediatric Emergency Medicine", "Pediatric Emergency Medicine", "Pediatric Endocrinology", "Pediatric Gastroenterology", "Pediatric Hematology-Oncology", "Pediatric Infectious Diseases", "Pediatric Nephrology", "Pediatric Orthopaedics", "Pediatric Otolaryngology", "Pediatric Pathology", "Pediatric Pulmonology", "Pediatric Radiology", "Pediatric Rheumatology", "Pediatric Sports Medicine", "Pediatric Surgery", "Pediatric Transplant Hepatology", "Pediatric Urology", "Pediatrics", "Physical Medicine & Rehabilitation", "Plastic Surgery", "Preventive Medicine", "Procedural Dermatology", "Psychiatry", "Pulmonary Disease", "Pulmonary Disease & Critical Care Medicine", "Radiation Oncology", "Radiology-Diagnostic", "Rheumatology", "Sleep Medicine", "Spinal Cord Injury Medicine", "Sports Medicine", "Sports Medicine", "Sports Medicine", "Sports Medicine", "Surgery-General", "Surgical Critical Care", "Thoracic Surgery", "Thoracic Surgery-Integrated", "Transplant Hepatology", "Urology", "Vascular & Interventional Radiology", "Vascular Surgery"]
+all_specialties.each { |spec| Specialty.create(name: spec)}
+cardiology = Specialty.find_by(name: 'Cardiology')
 
 fulano = User.new(first_name: 'Fulano', last_name: 'Branquinho', city: 'São Paulo',
   email: 'fulano@cadedoutor.com',password: 'fulano123', doctor: nil)
@@ -49,4 +53,37 @@ dr_beltrana.save!
 patient = User.create!(first_name: 'Futrano', last_name: 'Coelho', city: 'São Paulo',
   email: 'futrano@cadedoutor.com', password: 'futrano123', doctor: nil)
 
-puts 'Successful'
+
+
+puts 'fulano, ciclana, beltrana and futrano successfully created'
+puts '----------------------------------------------------------'
+puts 'populating database with 300 doctors'
+
+
+
+
+  300.times do
+    user = User.new(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name, city: 'São Paulo',
+    email: Faker::Internet.email, password: Faker::Internet.password(min_length: 8),doctor: nil)
+
+    doctor = Doctor.new(professional_register: "#{8.times { rand(9) }}",
+    address: Faker::Address.street_address, user: user)
+
+    random_specialty = Specialty.find(rand(1..115))
+    user.doctor = doctor
+    doctor.specialties = [random_specialty]
+    user.save!
+    doctor.save!
+
+    random_boolean = rand() > 0.3 ? true : false
+    random_day = rand(1..30)
+    random_hour = rand(1..12)
+    rand(1..8).times do
+      Offer.create(doctor: doctor, specialty: random_specialty, available: random_boolean,
+        start_date: DateTime.new(2019,8,random_day,random_hour,0),
+        end_date: DateTime.new(2019,8,random_day,random_hour + 1,0)
+      )
+    end
+  end
+
+puts 'database has been fully populated'
