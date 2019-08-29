8469a808a153222a957794d881300ea23548ad1a
Lucas Sandeville <lucas.sandeville@gmail.com>
2019-08-29 11:54:03 -0300

    implement essencial logic for infobox

--- app/javascript/components/doctor-infobox.js
+++ app/javascript/components/doctor-infobox.js
@@ -1,22 +1,45 @@
+const fetchJSONAndInsert = (docID, row) => {
+  fetch(`http://localhost:3000/doctors/${docID}.json`)
+    .then(response => response.json())
+    .then((data) => {
+      const doc = data.doctor;
+      insertHTML(row, doc);
+  });
+}
+
+const insertHTML = (row, doc) => {
+  row.insertAdjacentHTML('afterend',
+    `<div class="doctor-infobox">\
+      <div class="arrow"></div>\
+      <div class="doctor-box">\
+        <p>${doc.firstName}</p>\
+      </div>\
+    </div>`
+  );
+}
+
+const removeInfobox = () => {
+  const infobox = document.querySelector('.doctor-infobox');
+  if (infobox != null) {
+    infobox.remove();
+  }
+}
+
 const showDoctorInfobox = () => {
   const docCards = document.querySelectorAll('.doctor-card');
+
   docCards.forEach((docCard) => {
     docCard.addEventListener('click', (event) => {
       event.preventDefault();
-      const infobox = document.querySelector('.doctor-infobox');
-      if (infobox != null) {
-        infobox.remove();
-      }
+      removeInfobox();
 
+      const docID = docCard.dataset.id;
       const row = event.currentTarget.parentElement.parentElement
-      row.insertAdjacentHTML('afterend',
-        `<div class="doctor-infobox">\
-          <div class="arrow"></div>\
-          <div class="doctor-box"></div>\
-        </div>`
-      );
+      fetchJSONAndInsert(docID, row);
     });
   });
 }
+
+
 
 export { showDoctorInfobox };
